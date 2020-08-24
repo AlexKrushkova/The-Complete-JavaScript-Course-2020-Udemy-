ecaf91e090b51dfd414582dee134df3577065db9
alexkrushkova <alexkrushkova@gmail.com>
2020-08-24 18:34:12 +0200

    added income and expenses function constructors

--- 06. Budget App Project/app.js
+++ 06. Budget App Project/app.js
@@ -1,6 +1,28 @@
 var budgetController = (function(){
     
-    //Some Code
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+   var data = {
+       allItems: {
+           exp: [],
+           icn: []
+       },
+       totals: {
+           exp: 0,
+           inc: 0
+       }
+   }
 
 })();
 
